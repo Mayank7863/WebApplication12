89a6da90734edb2731ff39ee618274775db4b15c
Mayank Yadav <mayankyadav33@gmail.com>
2022-08-18 16:45:16 +0530

    One more change i = 1

--- Webtest/WebTest.cs
+++ Webtest/WebTest.cs
@@ -8,10 +8,10 @@ namespace Webtest
         [Fact]
         public void DemoTest()
         {
-            int i = 2;
+            int i = 1;
             bool result = false;
-            if (i == 2) result = true;
-            Assert.True(result, "Value should be equal to 2");
+            if (i == 1) result = true;
+            Assert.True(result, "Value should be equal to 1");
         }
     }
 }
